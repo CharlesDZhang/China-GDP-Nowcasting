fa1e5cce1a51b5a7bee10aacced84870f62e8905
zhangdongshuo <49156399+zhangdongshuo@users.noreply.github.com>
2023-01-04 23:03:35 +0800

    Create BVAR.py

---
+++ BVAR.py
@@ -0,0 +1,15 @@
+import sys
+import os
+import os.path
+import pandas as pd
+import numpy as np 
+from datetime import datetime
+from tqdm import tqdm
+import warnings
+warnings.filterwarnings("ignore")
+from WindPy import *  
+w.start()
+if w.isconnected():
+    print('Wind API 连接成功！')
+else:
+    print('Wind API 连接失败，请重试！')
