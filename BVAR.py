1e24ff2374c7be43b8671a1d35614d6782de79d7
zhangdongshuo <49156399+zhangdongshuo@users.noreply.github.com>
2023-01-11 23:22:37 +0800

    Update BVAR.py

--- BVAR.py
+++ BVAR.py
@@ -21,4 +21,40 @@ df['不变价:其他'] = 0
 df.loc[df[:92].index, '不变价:其他'] = df.loc[df[:92].index, '不变价:其']
 df.loc[df[92:].index, '不变价:其他'] = ((df['现价:信'] + df['现价:租']+ df['现价:其'])  / (df['现价:信'] / (df['不变价:信']/100+1) + df['现价:租'] / (df['不变价:租']/100+1)   + df['现价:其'] / (df['不变价:其']/100+1)) *100 -100 )[92:]
 df = df[['不变价:农', '不变价:工', '不变价:建', '不变价:批', '不变价:交', '不变价:住', '不变价:金', '不变价:房',  '不变价:其他']]
-df
+df_G['现价:其他'] =  df_G['现价:信'] + df_G['现价:租'] + df_G['现价:其'] 
+df_G = df_G[['现价:农', '现价:工', '现价:建', '现价:批', '现价:交', '现价:住', '现价:金', '现价:房', '现价:其他']]
+
+
+from rpy2.robjects import r
+from rpy2.robjects.packages import importr
+from rpy2.robjects import globalenv
+from rpy2.robjects import pandas2ri
+pandas2ri.activate()
+
+BVAR = importr("BVAR")
+df_pre = df[:].copy()
+df_pre= df_pre.append(pd.DataFrame([],index = [pd.to_datetime('2022-12-31')]))
+df_pre[:] = 0
+
+df_pre1 = df[:].copy()
+df_pre1=df_pre1.append(pd.DataFrame([],index = [pd.to_datetime('2022-12-31')]))
+df_pre1[:] = 0
+
+for i in tqdm(range(84,124)): 
+    rdf = pandas2ri.py2rpy(df.iloc[:i])
+    globalenv['rdf'] = rdf
+    rscript = """
+    bv_priors <- bv_mn( 
+    lambda = bv_lambda(mode = 0.6 ),
+    alpha = bv_alpha(mode = 0.4), 
+    var = 10000000, 
+    b =1
+    ) 
+    x <- bvar(rdf, lags = 6, n_draw = 1000L, n_burn = 200L, priors = bv_priors(), verbose = FALSE)
+    predict(x) <- predict(x, horizon = 2)
+    irf(x) <- irf(x, horizon = 2, fevd = FALSE)
+    summary(x)
+    """
+    result = r(rscript)
+    df_pre.iloc[i] = np.median(result[0][10][0] ,  axis = 0 )[0]
+    df_pre1.iloc[i] = np.median(result[0][10][0] ,  axis = 0 )[1]
